5f02d410baa2d0e6c6e665c59407ae59a7e4a7fd
Maggie <maggie.lusk26@gmail.com>
2026-02-25 14:17:15 +0000

    added while loops

--- 3-while-loops.py
+++ 3-while-loops.py
@@ -6,9 +6,28 @@ def main():
     the loop to repeat and it depends on another factor.
     """
     
+
+    #how many continents
+    answer = "7"
+    is_correct_answer = False
+
+    while not is_correct_answer: 
+        #ask the user to make a guess of the answer
+        guess=input("How many continents are there?")
+
+        #check if answer is correct
+        if int(guess) == int(answer):
+            print ("Yay, you got it correct!")
+            is_correct_answer = True
+        else:
+            print("You guessed:", guess)
+            print ("Nope, that's not right, try again.")
+        
+
     # guess the correct answer
     # WHILE answer is not correct loop
     # conditional inside while to check the answer
+
 
     pass
 
